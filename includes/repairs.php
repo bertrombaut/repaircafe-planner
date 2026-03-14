15491651b747c4662c491271436e0de333768c62
bertrombaut <bertrombaut14081966@gmail.com>
2026-03-14 11:52:04 +0100

    Update repairs.php

--- includes/repairs.php
+++ includes/repairs.php
@@ -1,1 +1,24 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
 
+function repaircafe_add_repair($data) {
+    global $wpdb;
+
+    $table = $wpdb->prefix . 'repaircafe_planner';
+
+    $wpdb->insert(
+        $table,
+        array(
+            'customer_name' => $data['customer_name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+            'device_type' => $data['device_type'],
+            'repair_description' => $data['repair_description'],
+            'appointment_date' => $data['appointment_date'],
+            'appointment_time' => $data['appointment_time'],
+            'status' => 'nieuw'
+        )
+    );
+}

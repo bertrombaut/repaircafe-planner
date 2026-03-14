f68c93e919ae1ba642b44620967d75448d031406
bertrombaut <bertrombaut14081966@gmail.com>
2026-03-14 13:15:21 +0100

    Update repairs.php

--- includes/repairs.php
+++ includes/repairs.php
@@ -39,40 +39,17 @@ function repaircafe_create_event( $data ) {
 
     $table = $wpdb->prefix . 'rcp_events';
 
-    $defaults = array(
-        'title'       => '',
-        'event_date'  => '',
-        'start_time'  => '',
-        'end_time'    => '',
-        'location'    => '',
-        'description' => '',
-    );
-
-    $data = wp_parse_args( $data, $defaults );
-
-    $inserted = $wpdb->insert(
+    $wpdb->insert(
         $table,
         array(
-            'title'       => sanitize_text_field( $data['title'] ),
-            'event_date'  => sanitize_text_field( $data['event_date'] ),
-            'start_time'  => sanitize_text_field( $data['start_time'] ),
-            'end_time'    => sanitize_text_field( $data['end_time'] ),
-            'location'    => sanitize_text_field( $data['location'] ),
-            'description' => sanitize_textarea_field( $data['description'] ),
-        ),
-        array(
-            '%s',
-            '%s',
-            '%s',
-            '%s',
-            '%s',
-            '%s',
+            'title'       => sanitize_text_field($data['title']),
+            'event_date'  => sanitize_text_field($data['event_date']),
+            'start_time'  => sanitize_text_field($data['start_time']),
+            'end_time'    => sanitize_text_field($data['end_time']),
+            'location'    => sanitize_text_field($data['location']),
+            'description' => sanitize_textarea_field($data['description']),
         )
     );
-
-    if ( false === $inserted ) {
-        return false;
-    }
 
     return $wpdb->insert_id;
 }
@@ -86,7 +63,84 @@ function repaircafe_delete_event( $event_id ) {
 
     return $wpdb->delete(
         $table,
-        array( 'id' => absint( $event_id ) ),
-        array( '%d' )
+        array('id' => absint($event_id))
     );
+}
+
+
+/*
+EXPERTISES
+*/
+
+function repaircafe_get_expertises() {
+
+    global $wpdb;
+
+    $table = $wpdb->prefix . 'rcp_expertises';
+
+    return $wpdb->get_results(
+        "SELECT * FROM {$table} ORDER BY name ASC"
+    );
+}
+
+
+function repaircafe_add_expertise( $name ) {
+
+    global $wpdb;
+
+    $table = $wpdb->prefix . 'rcp_expertises';
+
+    $wpdb->insert(
+        $table,
+        array(
+            'name' => sanitize_text_field($name)
+        )
+    );
+
+    return $wpdb->insert_id;
+}
+
+
+function repaircafe_get_user_expertises( $user_id ) {
+
+    global $wpdb;
+
+    $table = $wpdb->prefix . 'rcp_user_expertises';
+
+    return $wpdb->get_col(
+        $wpdb->prepare(
+            "SELECT expertise_id FROM {$table} WHERE user_id = %d",
+            $user_id
+        )
+    );
+}
+
+
+function repaircafe_set_user_expertises( $user_id, $expertises ) {
+
+    global $wpdb;
+
+    $table = $wpdb->prefix . 'rcp_user_expertises';
+
+    $wpdb->delete(
+        $table,
+        array('user_id' => $user_id)
+    );
+
+    if ( empty($expertises) ) {
+        return;
+    }
+
+    foreach ( $expertises as $expertise_id ) {
+
+        $wpdb->insert(
+            $table,
+            array(
+                'user_id'      => $user_id,
+                'expertise_id' => $expertise_id
+            )
+        );
+
+    }
+
 }

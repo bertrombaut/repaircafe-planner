0942c6eb16f007341a40bd070d7463cc7da9733f
bertrombaut <bertrombaut14081966@gmail.com>
2026-03-14 16:53:34 +0100

    Update repaircafe-planner.php
    - wijzigen in __

--- repaircafe-planner.php
+++ repaircafe-planner.php
@@ -7,9 +7,9 @@
 echo "PLUGIN TEST";
 if (!defined('ABSPATH')) exit;
 
-require_once plugin_dir_path(_FILE_) . 'includes/admin.php';
-require_once plugin_dir_path(_FILE_) . 'includes/database.php';
-require_once plugin_dir_path(_FILE_) . 'includes/repairs.php';
+require_once plugin_dir_path(__FILE__) . 'includes/admin.php';
+require_once plugin_dir_path(__FILE__) . 'includes/database.php';
+require_once plugin_dir_path(__FILE__) . 'includes/repairs.php';
 
 class RepairCafePlanner {
 

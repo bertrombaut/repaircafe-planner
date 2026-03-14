3b2cbccdd7d7b5a32932463a604591f650e94910
bertrombaut <bertrombaut14081966@gmail.com>
2026-03-14 12:58:32 +0100

    Update admin.php

--- includes/admin.php
+++ includes/admin.php
@@ -1,6 +1,10 @@
 <?php
 if ( ! defined('ABSPATH') ) exit;
 
+add_action('admin_menu', 'rcp_admin_menu');
+add_action('admin_menu', 'repaircafe_admin_menu');
+
+
 function rcp_admin_menu() {
 
     add_submenu_page(
@@ -20,7 +24,34 @@ function rcp_admin_menu() {
         'rc_settings',
         'rcp_settings_page'
     );
+
 }
+
+
+function repaircafe_admin_menu() {
+
+    add_menu_page(
+        'Repair Café',
+        'Repair Café',
+        'read',
+        'repaircafe',
+        'repaircafe_admin_events_page',
+        'dashicons-hammer',
+        26
+    );
+
+    add_submenu_page(
+        'repaircafe',
+        'Reparatiedagen',
+        'Reparatiedagen',
+        'read',
+        'repaircafe',
+        'repaircafe',
+        'repaircafe_admin_events_page'
+    );
+
+}
+
 
 function rcp_settings_page() {
 
@@ -30,10 +61,21 @@ function rcp_settings_page() {
 
 }
 
+
 function rcp_admin_signups_page() {
 
     echo '<div class="wrap"><h1>Aanmeldingen</h1>';
     echo '<p>Admin pagina tijdelijk actief.</p>';
+    echo '</div>';
+
+}
+
+
+function repaircafe_admin_events_page() {
+
+    echo '<div class="wrap">';
+    echo '<h1>Reparatiedagen</h1>';
+    echo '<p>Events worden hier straks beheerd.</p>';
     echo '</div>';
 
 }

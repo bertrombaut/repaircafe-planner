84556da73e754831bfc13e0d132a80215ed93c36
bertrombaut <bertrombaut14081966@gmail.com>
2026-03-27 14:44:17 +0100

    Update database.php
    deze versie is voorzien van documentatie

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,27 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
+/**
+ * Maakt of werkt alle extra plugin-tabellen bij.
+ *
+ * Doet:
+ * - laadt dbDelta
+ * - maakt tabellen voor:
+ *   - globale expertises
+ *   - koppeling gebruiker ↔ expertise
+ *   - koppeling event ↔ expertise met maximum vrijwilligers
+ * - voert elke CREATE TABLE query uit
+ *
+ * In:
+ * - niets
+ *
+ * Uit:
+ * - niets
+ *
+ * Waarom zo gebouwd:
+ * - alle extra tabellen staan centraal in 1 functie
+ * - dbDelta kan veilig aanmaken en bijwerken
+ */
 function repaircafe_create_tables() {
     global $wpdb;
 
@@ -13,7 +34,7 @@ function repaircafe_create_tables() {
 
     $sql = [];
 
-    // Expertises (globaal)
+    // Tabel met alle beschikbare expertises die globaal in de plugin gebruikt worden.
     $sql[] = "CREATE TABLE $expertises_table (
         id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
         name VARCHAR(100) NOT NULL,
@@ -22,7 +43,8 @@ function repaircafe_create_tables() {
         UNIQUE KEY name (name)
     ) $charset_collate;";
 
-    // User expertises
+    // Koppelt gebruikers aan 1 of meer expertises.
+    // UNIQUE KEY voorkomt dat dezelfde expertise dubbel aan dezelfde gebruiker hangt.
     $sql[] = "CREATE TABLE $user_expertises_tbl (
         id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
         user_id BIGINT UNSIGNED NOT NULL,
@@ -34,7 +56,8 @@ function repaircafe_create_tables() {
         KEY expertise_id (expertise_id)
     ) $charset_collate;";
 
-    // Event expertises + max
+    // Koppelt expertises aan een specifiek event.
+    // Hier wordt ook per expertise het maximum aantal vrijwilligers opgeslagen.
     $sql[] = "CREATE TABLE $event_expertises_tbl (
         id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
         event_id BIGINT UNSIGNED NOT NULL,
